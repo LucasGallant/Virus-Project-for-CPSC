c9ca8b319484f6fafdb2e0bb9e367f9fb5b5f929
Zac Altland <zacaltland@gmail.com>
2026-04-17 13:51:27 -0400

    cool changes, prompt popup

--- you_exe.py
+++ you_exe.py
@@ -1,7 +1,8 @@
-import cv2, os, sys, keyboard, threading
+import cv2, os, sys, keyboard, threading, random
 from tkinter import *
 from pathlib import Path
 import tkinter as tk
+
 
 def initialize():
     os.chdir(Path(__file__).parent)
@@ -9,19 +10,33 @@ def initialize():
     pass
 
 def startUp():
+
     initialize()
     t1 = threading.Thread(target=display_image_unclosable)
     t2 = threading.Thread(target=always_on)
     # Take picture of the user
-    take_picture()
+    #take_picture()
     t1.start()
     t2.start()
-
 
 def always_on():
     while True:
         if(keyboard.is_pressed('p')):
             os._exit(0)
+
+        # we are going to have to use a seperate thread for this or else our keybind will never be listened too.
+        if(mood == 0):
+            prompt_popups(0)
+        if(mood == 1):
+            prompt_popups(1)
+        if(mood == 2):
+            prompt_popups(2)
+        else:
+            prompt_popups(3)
+
+
+        
+    
 
 
 def take_picture():
@@ -43,13 +58,36 @@ def take_picture():
 
 
 def display_image_unclosable():
+    global mood
+    mood = 0
     while True:
         root = Tk()
         root.geometry("500x400")
         photo = PhotoImage(file="_image.png", height=500, width=500)
         label = Label(root, image = photo)
         label.pack()
-        if keyboard.is_pressed('p'):
-            print("Key pressed!")
+        #prompt_popups()
         root.mainloop()
+
+def prompt_popups(indicator):
+
+    friendly_prompts = ["Hello I am you.exe!", "I am you!", "Good day!", "Hello there"]
+    agitated_prompts = ["Thats not very nice", "I thought we were friends", "Why would you do that?", "I am not happy with you"]
+    Angry_prompts = ["I am very angry with you", "How could you do that?", "You were my friend."]
+    Meltdown_prompts = ["Total destruction", "Your PC is mine", "I control you", "I am you"]
+
+    if indicator == 0:
+
+        top = Toplevel()
+        top.geometry("500x400")
+        label = Label(top, text=random.choice(friendly_prompts))
+        label.pack()
+        top.mainloop()
+    if indicator == 2:
+        pass
+    if indicator == 3:
+        pass
+    else:
+        pass
+
 
