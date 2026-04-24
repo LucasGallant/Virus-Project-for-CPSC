37b0c46bd8a7fdbbad22d39a2fc358113a390c95
Zac Altland <zacaltland@gmail.com>
2026-04-23 23:59:56 -0400

    Added guideline comments

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import desktopChange as dc
 import time
 
 
+# On startup
 you.startUp()
 
-
+# wait until mood > 2
 while True:
     time.sleep(.5)
     print(you.get_mood())
     if you.get_mood() >= 3:
-        dc.attackStart()
+        # Use functions defined in desktopChange.py here, with respect to order and timing
+
         break
 
 print("pen test complete!")
